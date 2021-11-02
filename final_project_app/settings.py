183ad1c48b403663c15d4d93d00cb183417e221e
nhật vũ <nhatvmgcs18725@fpt.edu.vn>
2021-11-02 17:51:03 +0700

    o2

--- final_project_app/settings.py
+++ final_project_app/settings.py
@@ -232,8 +232,7 @@ RECAPTCHA_PRIVATE_KEY = config('RECAPTCHA_PRIVATE_KEY')
 RECAPTCHA_PUBLIC_KEY = config('RECAPTCHA_PUBLIC_KEY')
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000/', 'http://127.0.0.1:3000/',
-                        'https://letsgo-front-end.herokuapp.com/', 'http://letsgo-front-end.herokuapp.com/', 'http://localhost:3000/', 'http://127.0.0.1:3000/',
-                        'https://letsgo-front-end.herokuapp.com/', 'http://letsgo-front-end.herokuapp.com/']
+                        'https://letsgo-front-end.herokuapp.com/', 'http://letsgo-front-end.herokuapp.com/', ]
 
 """EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'"""
 
